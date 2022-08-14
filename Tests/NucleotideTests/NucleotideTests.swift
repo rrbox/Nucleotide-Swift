acdb9e3671bf0e368e8e7dd33dac0596d26e93ed
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-15 05:21:32 +0900

    [fix] test のコメントアウトを戻した

--- Tests/NucleotideTests/NucleotideTests.swift
+++ Tests/NucleotideTests/NucleotideTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import Nucleotide
+@testable import Nucleotide
 
 final class NucleotideTests: XCTestCase {
     var seq: BaseSequence<DNA>?
@@ -30,12 +30,12 @@ final class NucleotideTests: XCTestCase {
     }
 }
 
-//final class HelperTests: XCTestCase {
-//    func testBitRotate() throws {
-//        let target = UInt8(0b00000011)
-//        XCTAssertEqual(target, bitRotateLeft(0b10000001, n: 1))
-//        XCTAssertEqual(target, bitRotateLeft(0b11000000, n: 2))
-//        XCTAssertEqual(target, bitRotateLeft(target, n: 8))
-//    }
-//    
-//}
+final class HelperTests: XCTestCase {
+    func testBitRotate() throws {
+        let target = UInt8(0b00000011)
+        XCTAssertEqual(target, bitRotateLeft(0b10000001, n: 1))
+        XCTAssertEqual(target, bitRotateLeft(0b11000000, n: 2))
+        XCTAssertEqual(target, bitRotateLeft(target, n: 8))
+    }
+    
+}
