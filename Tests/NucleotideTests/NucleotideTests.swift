537b4d302db8aa69511b121eaefe0b3f1e16826e
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-14 22:10:56 +0900

    [add] helper test

--- Tests/NucleotideTests/NucleotideTests.swift
+++ Tests/NucleotideTests/NucleotideTests.swift
@@ -29,3 +29,13 @@ final class NucleotideTests: XCTestCase {
         
     }
 }
+
+final class HelperTests: XCTestCase {
+    func testBitRotate() throws {
+        let target = UInt8(0b00000011)
+        XCTAssertEqual(target, bitRotateLeft(0b10000001, n: 1))
+        XCTAssertEqual(target, bitRotateLeft(0b11000000, n: 2))
+        XCTAssertEqual(target, bitRotateLeft(target, n: 8))
+    }
+    
+}
