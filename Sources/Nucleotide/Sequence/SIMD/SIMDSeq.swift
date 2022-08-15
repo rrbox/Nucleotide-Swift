950500473b39269fef4e0b35cab5463e3cc7a9be
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-16 02:26:52 +0900

    [add] BaseSequenceSIMD64

--- Sources/Nucleotide/Sequence/SIMD/SIMDSeq.swift
+++ Sources/Nucleotide/Sequence/SIMD/SIMDSeq.swift
@@ -5,4 +5,34 @@
 //  Created by rrbox on 2022/08/16.
 //
 
-import Foundation
+public struct BaseSequenceSIMD64<T: BaseType>: CustomStringConvertible {
+    var sequence: [SIMD64<UInt8>]
+    
+    init(sequence: [UInt8]) {
+        self.sequence = stride(from: sequence.startIndex, through: sequence.endIndex, by: 64).map { i -> SIMD64<UInt8> in
+            return SIMD64<UInt8>(
+                (i ..< i+64).map { i -> UInt8 in
+                    if sequence.indices.contains(i) {
+                        return sequence[i]
+                    } else {
+                        return .zero
+                    }
+                }
+            )
+        }
+    }
+    
+    func description(simd: SIMD64<UInt8>) -> [Character] {
+        (0 ..< 64).map { i in
+            T.baseToUnicodeDict[simd[i]]!
+        }
+    }
+    
+    public var description: String {
+        "SIMD64 DNA: " + String(self.sequence.reduce(into: []) { partialResult, simd in
+            partialResult += self.description(simd: simd)
+        })
+    }
+    
+}
+
