f479c046fe85c71883322c687a99bc845659cd09
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-16 02:30:42 +0900

    [clean] test

--- Tests/NucleotideTests/APITests/SIMDTests/SIMDSeqTests.swift
+++ Tests/NucleotideTests/APITests/SIMDTests/SIMDSeqTests.swift
@@ -6,7 +6,32 @@
 //
 
 import XCTest
+import Nucleotide
 
-class SIMDSeqTests: XCTestCase {
+final class SIMD64SequenceTests: XCTestCase {
+    func testPerformanceGenerateSIMD64Seq() throws {
+        let seq = BaseSequence<DNA>.init(stringLiteral: String(repeating: "A", count: 1000000))
+        self.measure {
+            _ = BaseSequenceSIMD64<DNA>(seq: seq)
+        }
+//        結果
+//        10万 bp: 0.065 sec
+//        1000万 bp: 0.497 sec
+//        4000万 bp: 1.988 sec
+        
+        print()
+        print(BaseSequenceSIMD64<DNA>(seq: BaseSequence<DNA>.init(stringLiteral: String(repeating: "ATGG", count: 65))))
+        print()
+        print(BaseSequence<DNA>.init(stringLiteral: String(repeating: "ATGG", count: 65)))
+        print()
+    }
     
+    func testPerformanceDescription() throws {
+        let seq = BaseSequence<DNA>.init(stringLiteral: String(repeating: "A", count: 1000000))
+        let simdSeq = BaseSequenceSIMD64<DNA>(seq: seq)
+        self.measure {
+            _ = simdSeq.description
+        }
+//        1000万 pb: 0.376 sec
+    }
 }
