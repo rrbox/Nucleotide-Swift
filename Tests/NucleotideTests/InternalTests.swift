c67c3d4ec53e2f03abc5643c449a1e5d942f700f
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-16 02:54:40 +0900

    [add] SIMD 向けのビット回転関数

--- Tests/NucleotideTests/InternalTests.swift
+++ Tests/NucleotideTests/InternalTests.swift
@@ -9,4 +9,12 @@ final class HelperTests: XCTestCase {
         XCTAssertEqual(target, bitRotateLeft(target, n: 8))
     }
     
+    func testSIMDBitRotate() throws {
+        let target = SIMD64(repeating: UInt8(0b00000011))
+        XCTAssertEqual(target, bitRotateLeft(SIMD64(repeating: 0b10000001), n: 1))
+        XCTAssertEqual(target, bitRotateLeft(SIMD64(repeating: 0b11000000), n: 2))
+        XCTAssertEqual(target, bitRotateLeft(target, n: 8))
+        print(target)
+    }
+    
 }
