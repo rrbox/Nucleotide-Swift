1729231eef5206e65b04dc9570f8bc36ca59809c
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-19 06:25:42 +0900

    [update] SIMD を使った配列の生成速度を高速化した

--- Sources/Nucleotide/Sequence/Declarations/SIMDSeq.swift
+++ Sources/Nucleotide/Sequence/Declarations/SIMDSeq.swift
@@ -5,6 +5,8 @@
 //  Created by rrbox on 2022/08/16.
 //
 
+import simd
+
 /// SIMD 64 で表現された配列を作成します.
 ///
 /// 相補鎖の生成, 標準出力の表示などを BaseSequence よりも高速に行うことができます.
@@ -12,16 +14,11 @@ public struct BaseSequenceSIMD64<T: BaseType>: CustomStringConvertible {
     var sequence: [SIMD64<UInt8>]
     
     init(uint8 sequence: [UInt8]) {
+        // 0 を後ろに埋めて要素数を 64 の倍数にする
+        // - note: 最初から 64 の倍数の場合, 後ろにギャップが64個追加されてしまいます.
+        let seq = sequence + [UInt8](repeating: 0, count: 64 - sequence.count%64)
         self.sequence = stride(from: sequence.startIndex, through: sequence.endIndex, by: 64).map { i -> SIMD64<UInt8> in
-            return SIMD64<UInt8>(
-                (i ..< i+64).map { i -> UInt8 in
-                    if sequence.indices.contains(i) {
-                        return sequence[i]
-                    } else {
-                        return .zero
-                    }
-                }
-            )
+            return simd_uchar64(seq[i..<i+64])
         }
     }
     
--- Tests/NucleotideTests/APITests/SIMDTests/SIMDSeqTests.swift
+++ Tests/NucleotideTests/APITests/SIMDTests/SIMDSeqTests.swift
@@ -10,14 +10,14 @@ import Nucleotide
 
 final class SIMD64SequenceTests: XCTestCase {
     func testPerformanceGenerateSIMD64Seq() throws {
-        let seq = BaseSequence<DNA>.init(stringLiteral: String(repeating: "A", count: 10000000))
+        let seq = BaseSequence<DNA>.init(stringLiteral: String(repeating: "A", count: 100000))
         self.measure {
             _ = BaseSequenceSIMD64<DNA>(seq: seq)
         }
 //        結果
-//        10万 bp: 0.065 sec
-//        100万 bp: 0.497 sec
-//        1000万 bp: 4.942 sec
+//        10万 bp: 0.012 sec
+//        100万 bp: 0.096 sec
+//        1000万 bp: 0.944 sec
         
         print()
         print(BaseSequenceSIMD64<DNA>(seq: BaseSequence<DNA>.init(stringLiteral: String(repeating: "ATGG", count: 65))))
