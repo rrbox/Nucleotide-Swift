2b5dd80f70599c4a918d6a74492b65f9b371b0b7
rrbox <87851278+rrbox@users.noreply.github.com>
2022-08-22 08:31:04 +0900

    [update] Seq Generator

--- Sources/Nucleotide/Sequence/Declarations/Sequence.swift
+++ Sources/Nucleotide/Sequence/Declarations/Sequence.swift
@@ -6,17 +6,17 @@
 //
 
 public protocol BaseType {
-    static var unicodeToBaseDict: [Unicode.UTF8.CodeUnit: UInt8] { get }
+    static var unicodeToBaseDict: [UInt32: UInt8] { get }
     static var baseToUnicodeDict: [UInt8: Character] { get }
 }
 
 public enum DNA: BaseType {
-    public static let unicodeToBaseDict: [Unicode.UTF8.CodeUnit: UInt8] = [
-        UnicodeScalar("A").utf8.first!: Nucleotide.a.rawValue,
-        UnicodeScalar("C").utf8.first!: Nucleotide.c.rawValue,
-        UnicodeScalar("G").utf8.first!: Nucleotide.g.rawValue,
-        UnicodeScalar("T").utf8.first!: Nucleotide.t.rawValue,
-        UnicodeScalar("N").utf8.first!: Nucleotide.n.rawValue,
+    public static let unicodeToBaseDict: [UInt32: UInt8] = [
+        UnicodeScalar("A").value: Nucleotide.a.rawValue,
+        UnicodeScalar("C").value: Nucleotide.c.rawValue,
+        UnicodeScalar("G").value: Nucleotide.g.rawValue,
+        UnicodeScalar("T").value: Nucleotide.t.rawValue,
+        UnicodeScalar("N").value: Nucleotide.n.rawValue,
     ]
     
     public static let baseToUnicodeDict: [UInt8: Character] = [
@@ -31,12 +31,12 @@ public enum DNA: BaseType {
 }
 
 public enum RNA: BaseType {
-    public static let unicodeToBaseDict: [Unicode.UTF8.CodeUnit: UInt8] = [
-        UnicodeScalar("A").utf8.first!: Nucleotide.a.rawValue,
-        UnicodeScalar("C").utf8.first!: Nucleotide.c.rawValue,
-        UnicodeScalar("G").utf8.first!: Nucleotide.g.rawValue,
-        UnicodeScalar("U").utf8.first!: Nucleotide.u.rawValue,
-        UnicodeScalar("N").utf8.first!: Nucleotide.n.rawValue,
+    public static let unicodeToBaseDict: [UInt32: UInt8] = [
+        UnicodeScalar("A").value: Nucleotide.a.rawValue,
+        UnicodeScalar("C").value: Nucleotide.c.rawValue,
+        UnicodeScalar("G").value: Nucleotide.g.rawValue,
+        UnicodeScalar("U").value: Nucleotide.u.rawValue,
+        UnicodeScalar("N").value: Nucleotide.n.rawValue,
     ]
     
     public static let baseToUnicodeDict: [UInt8: Character] = [
@@ -54,8 +54,8 @@ public struct BaseSequence<T: BaseType>: ExpressibleByStringLiteral, CustomStrin
     var sequence: [UInt8]
     
     public init(stringLiteral value: StringLiteralType) {
-        self.sequence = value.utf8.map({ s in
-            T.unicodeToBaseDict[s]!
+        self.sequence = value.unicodeScalars.map({ s in
+            T.unicodeToBaseDict[s.value]!
         })
     }
     
